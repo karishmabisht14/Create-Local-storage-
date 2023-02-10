0d6e88c7c0520f899f81ca96be7a6788155854bc
karishmabisht14 <karishma.qt14@gmail.com>
2023-02-10 20:10:51 +0530

    Task 13 delete data from DOM and local storage

--- Task 12/main.js
+++ Task 12/main.js
@@ -13,13 +13,19 @@ form.addEventListener('submit', (e) => {
 
     //create li for the ul
     const li = document.createElement('li');
+    // create delete btn
+    const deleteBtn = document.createElement('button');
 
-    //create text node
+    //create text node to li
     li.appendChild(document.createTextNode(`${nameInput.value} - ${emailInput.value} - ${phoneInput.value}`));
+    //create text node to button
+    deleteBtn.appendChild(document.createTextNode("Delete"))
 
+    //append that deleteBtn to li
+    li.appendChild(deleteBtn);
     //append that li to ul
     ul.appendChild(li);
-
+    
     //insert it into html
     form.insertAdjacentElement('afterend', ul);
 
@@ -32,13 +38,28 @@ form.addEventListener('submit', (e) => {
 
     //convert it into Json format
     let userDetailsJson = JSON.stringify(userDetails);
-    console.log(userDetailsJson);
 
     //create localStorage
     localStorage.setItem( emailInput.value , userDetailsJson);
+
+    //storing the emailInput value
+    let text = emailInput.value;
+    
+    //delete button event
+    deleteBtn.addEventListener('click', (e) => {
+        //delete the li from ul
+        ul.removeChild(li);
+
+        //delete data from local storage
+        localStorage.removeItem(text);
+        
+    });
 
     //clear fields
     nameInput.value = '';
     emailInput.value = '';
     phoneInput.value = '';
+   
 });
+
+
