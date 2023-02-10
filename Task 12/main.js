2d909b3c0b11d35296c1372be398d9fbed8dfc58
karishmabisht14 <karishma.qt14@gmail.com>
2023-02-10 20:30:15 +0530

    Task 14 creating edit button

--- Task 12/main.js
+++ Task 12/main.js
@@ -13,14 +13,20 @@ form.addEventListener('submit', (e) => {
 
     //create li for the ul
     const li = document.createElement('li');
+    //create edit btn
+    const editBtn = document.createElement('button');
     // create delete btn
     const deleteBtn = document.createElement('button');
 
     //create text node to li
     li.appendChild(document.createTextNode(`${nameInput.value} - ${emailInput.value} - ${phoneInput.value}`));
-    //create text node to button
+    //create tect node to edit btn
+    editBtn.appendChild(document.createTextNode('Edit'));
+    //create text node to delete button
     deleteBtn.appendChild(document.createTextNode("Delete"))
 
+    //append that editBtn to li
+    li.appendChild(editBtn);
     //append that deleteBtn to li
     li.appendChild(deleteBtn);
     //append that li to ul
@@ -42,16 +48,35 @@ form.addEventListener('submit', (e) => {
     //create localStorage
     localStorage.setItem( emailInput.value , userDetailsJson);
 
-    //storing the emailInput value
-    let text = emailInput.value;
+    //storing the Inputs value
+    let nameText = nameInput.value;
+    let emailText = emailInput.value;
+    let phoneText = phoneInput.value;
     
+    //edit button event
+    editBtn.addEventListener('click', (e) => {
+
+        //displaying the input values
+        nameInput.value = nameText;
+        emailInput.value = emailText;
+        phoneInput.value = phoneText;
+        //delete the li from ul
+        ul.removeChild(li);
+        //delete data from local storage
+        localStorage.removeItem(emailText);    
+    });
+
     //delete button event
     deleteBtn.addEventListener('click', (e) => {
         //delete the li from ul
         ul.removeChild(li);
 
         //delete data from local storage
-        localStorage.removeItem(text);
+        localStorage.removeItem(emailText);
+        //clear fields
+        nameInput.value = '';
+        emailInput.value = '';
+        phoneInput.value = '';
         
     });
 
